8636129186da9954e69179b30f7e4c8c624d5f76
Zack <zack@ukaton.com>
2023-10-16 22:24:39 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UkatonKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UkatonKit",
+            targets: ["UkatonKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UkatonKit"),
+        .testTarget(
+            name: "UkatonKitTests",
+            dependencies: ["UkatonKit"]),
+    ]
+)
